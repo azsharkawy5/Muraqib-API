811823a543ac2cfeaea2f49c2c29fa93925d597e
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-05-24 14:58:49 +0300

    feat: import app module and ensure BigInt polyfill is loaded

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+import './src/config/bigint-polyfill.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import app from './src/app.js';
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
