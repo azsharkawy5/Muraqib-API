82379db88bbbd805ff2dc0b37c05be185193f63e
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-05-26 14:26:46 +0300

    feat: add authenticateToken middleware for JWT authentication

---
+++ src/middlewares/authenticateToken.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+
+const { JWT_SECRET } = process.env;
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: 'Authentication token is required' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, userPayload) => {
+    if (err) {
+      console.error('JWT verification error:', err.message);
+
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({
+          message: 'Token expired',
+        });
+      }
+
+      return res.status(403).json({
+        message: 'Token is not valid',
+      });
+    }
+    req.user = userPayload;
+    next();
+  });
+};
