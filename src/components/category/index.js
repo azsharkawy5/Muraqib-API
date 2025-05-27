74270169e6ce989dce4ce94d5a91d75f91ef6ee9
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-05-27 21:46:02 +0300

    feat: create index file for category components exports

---
+++ src/components/category/index.js
@@ -0,0 +1,4 @@
+export { default as categoryRepository } from './category.repository.js';
+export { default as categoryService } from './category.service.js';
+export { default as categoryController } from './category.controller.js';
+export { default as categoryValidator } from './category.validator.js';
