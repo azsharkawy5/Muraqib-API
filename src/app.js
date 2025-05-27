7ba72a4f6b7692063d9f4a97663ead694cf91bcf
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-05-27 21:50:27 +0300

    feat: set up routes for API versioning

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { errorMiddleware } from './middlewares/index.js';
-
+import routes from './routes/index.js';
 const app = express();
 
 // Middlewares
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Routes
+app.use('/api/v1', routes);
 // Error handling middleware
 app.use(errorMiddleware);
 
