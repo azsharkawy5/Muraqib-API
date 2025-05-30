aa323d1851a3f5fcb33e02fb97d218ee886587a0
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-05-30 15:54:02 +0300

    feat: add minimal authentication routes for user registration, login, and retrieval

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { validate } from '../middlewares/index.js';
+import { authController } from '../components/auth/index.js';
+import { authValidator } from '../components/auth/index.js';
+import { authenticateToken } from '../middlewares/authenticateToken.js';
+const router = Router();
+
+router.get('/me', authenticateToken, authController.getUserByEmail);
+
+router.post(
+  '/register',
+  validate(authValidator.createUserSchema),
+  authController.register
+);
+
+router.post(
+  '/login',
+  validate(authValidator.loginUserSchema),
+  authController.login
+);
+
+router.get('/', authController.getTenRandomUsers);
+
+export default router;
