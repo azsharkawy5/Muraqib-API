4a0549e2746ec2b20a485d992fff91e6e5ce5eb0
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-06-03 14:05:12 +0300

    feat: add subscription module exports for repository, service, controller, and validator

---
+++ src/components/subscription/index.js
@@ -0,0 +1,4 @@
+export { default as subscriptionRepository } from './subscription.repository.js';
+export { default as subscriptionService } from './subscription.service.js';
+export { default as subscriptionController } from './subscription.controller.js';
+export { default as subscriptionValidator } from './subscription.validator.js';
