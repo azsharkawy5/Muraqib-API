5954ccb8e7c710f18c2be2219ab163ceac667e35
Ahmad Zakaria <azsharkawy5@gmail.com>
2025-06-05 17:44:20 +0300

    feat: add subscription routes to main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import categoryRoutes from './category.routes.js';
 import authRoutes from './auth.routes.js';
-
+import subscriptionRoutes from './subscription.routes.js';
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/subscriptions', subscriptionRoutes);
 
 export default router;
